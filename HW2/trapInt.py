63faf5b7d5a0acf6eb0193f5b6ed86b98585db1b
Matt Wright <102613644+matt-wright2001@users.noreply.github.com>
2023-09-05 09:56:05 -0500

    HW2
    Simpson's rule created.  Sample data file created

---
+++ HW2/trapInt.py
@@ -0,0 +1,44 @@
+#
+# integration errors for trapezoid integration
+# RT Clay 2023
+#
+from math import sqrt
+# the function to integrate
+def f(x):
+    return sqrt(x)
+# trapezoid rule integration
+# n : number of integration points
+# min : lower limit
+# max : uppper limit
+#
+# a function f(x) must be defined
+def trap(n, min, max):
+    dx = (max - min) / (n-1)
+    sum = 0.0
+    # sum points inside interval
+    for i in range(1, n - 1):
+        x = min + dx * i
+        sum += f(x)
+    # add endpoints
+    sum += (f(min) + f(max))/2
+    sum *= dx
+    return sum
+# main program
+# limits and exact value of integral (used to calculate error)
+#
+# int_1^2 \sqrt{x}dx = \frac{2}{3}(2^{3/2} - 1)
+#
+a = 1.0
+b = 2.0
+exact = 2.0/3.0*(2**(1.5) - 1.0)
+
+for i in range(3,10000,2):
+    r = trap(i, a, b)
+    r = abs(r - exact)
+    print('{:6d} {:.12e}'.format(i,r))
+
+# for larger N, skip some values to save time
+for i in range(1001,3000000,10000):
+    r = trap(i, a, b)
+    r = abs(r - exact)
+    print('{:6d} {:.12e}'.format(i,r))
